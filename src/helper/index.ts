1e1567628288ce847eeab09153af6939b4a508d7
yongsin0129 <yongsin0129@gmail.com>
2022-12-10 20:39:11 +0800

    feat : implement queryResolvers

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,11 +1,40 @@
 import * as db from '../database'
+import * as Type from '../generated/graphql'
 
 // helper functions
 export const filterPostsByUserId = (userId: string) =>
   db.posts.filter(post => userId === post.authorId)
 
-export const filterUsersByUserIds = (userIds: string[]) =>
+export const filterUsersByUserIds = (userIds: (string | null)[]) =>
   db.users.filter(user => userIds.includes(user.id))
 
-export const findUserByUserId = (userId: string) =>
-  db.users.find(user => user.id === userId)
+export const findUserByUserId = (userId: string) => {
+  return db.users.find(user => user.id === userId) || null
+}
+
+export const findUserByName = (name: string) =>
+  db.users.find(user => user.name === name) || null
+  
+export const findPostByPostId = (postId: string) =>
+  db.posts.find(post => post.id === postId) || null
+
+// 實作 typeof 的 user-defined type guard
+export function isTypeof<T> (value: unknown, prim: T): value is T {
+  if (prim === null) {
+    return value === null
+  }
+  return value !== null && typeof prim === typeof value
+}
+
+// 實作 isDefined 的 user-defined type guard
+// 如果 arg 沒加上 undefined，則 filtered 的型別會推導成 (string | undefined)[]
+// 但若是使用 arg: T | undefined，則 undefined 會從該型別被抽出，因此最終會推導成 string[]
+export function isDefined<T> (arg: T | undefined): arg is T {
+  return typeof arg !== 'undefined'
+}
+
+// 實作 isPresent 的 user-defined type guard
+// Array filter 後的元素不會是 undefined 或 null
+export function isPresent<T> (arg: T | undefined | null): arg is T {
+  return arg !== null && typeof arg !== 'undefined'
+}
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,17 +1,33 @@
-const resolvers = {
-  // Query: {
-  //   hello: () => 'world'
-  // },
-  // User: {
-  //   posts: (parent, args, context) => filterPostsByUserId(parent.id),
-  //   friends: (parent, args, context) =>
-  //     filterUsersByUserIds(parent.friendIds || [])
-  // },
-  // Post: {
-  //   author: (parent, args, context) => findUserByUserId(parent.authorId),
-  //   likeGivers: (parent, args, context) =>
-  //     filterUsersByUserIds(parent.likeGiverIds)
-  // }
+import * as Type from '../generated/graphql'
+import * as helper from '../helper'
+import * as db from '../database'
+
+const resolvers: Type.Resolvers = {
+  Query: {
+    hello: () => 'world',
+    me: () => helper.findUserByUserId(db.meId),
+    users: () => db.users,
+    user: (root, { name }, context) => helper.findUserByName(name),
+    posts: () => db.posts,
+    post: (root, { id }, context) => helper.findPostByPostId(id)
+  },
+  User: {
+    posts: (parent, args, context) => helper.filterPostsByUserId(parent.id),
+    friends: (parent, args, context) => {
+      // if (!parent.friendIds) return []
+      return helper.filterUsersByUserIds(parent.friendIds || [])
+    }
+  },
+  Post: {
+    author: (parent, args, context) => {
+      if (!parent.authorId) return null
+      return helper.findUserByUserId(parent.authorId)
+    },
+    likeGivers: (parent, args, context) => {
+      // if (!parent.likeGiverIds) return []
+      return helper.filterUsersByUserIds(parent.likeGiverIds || [])
+    }
+  }
 }
 
 export default resolvers
